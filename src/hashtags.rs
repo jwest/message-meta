306c046b888a4dd83b3df3a983d4c5677492da90
Jakub Westfalewski <j.westfalewski@gmail.com>
2019-03-06 17:23:11 +0100

    add hashtag parsing feature (#2)
    * add hashtag parsing feature
    
    * change test as integration all features
    
    * add unit test for hashtags with only numbers
    
    * bump version to 0.2.0

---
+++ src/hashtags.rs
@@ -0,0 +1,64 @@
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Hashtag {
+    pub name: String,
+}
+
+pub fn parse<T: ToString>(msg: T) -> Vec<Hashtag> {
+    let message = msg.to_string();
+    let mut hashtags: Vec<Hashtag> = Vec::new();
+
+    let re = Regex::new(r###"\B#\w*[a-zA-Z]+\w*"###).unwrap();
+ 
+    for caps in re.captures_iter(&*message) {
+        hashtags.push(Hashtag { name: caps.get(0).unwrap().as_str().to_string() });
+    }
+
+    hashtags
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse;
+    // @TODO https://github.com/BurntSushi/quickcheck
+
+    #[test]
+    fn should_not_find_hashtag() {
+        let hashtags = parse("test message");
+
+        assert_eq!(hashtags.len(), 0);
+    }
+
+    #[test]
+    fn should_find_simply_hashtag() {
+        let hashtags = parse("#hashtag");
+
+        assert_eq!(hashtags.len(), 1);
+        assert_eq!(hashtags[0].name, "#hashtag");
+    }
+
+    #[test]
+    fn should_not_find_hashtag_because_contains_only_numbers() {
+        let hashtags = parse("#1");
+
+        assert_eq!(hashtags.len(), 0);
+    }
+
+    #[test]
+    fn should_find_hashtag_with_other_text() {
+        let hashtags = parse("text https://google.com/?test#hash #hashtag text");
+
+        assert_eq!(hashtags.len(), 1);
+        assert_eq!(hashtags[0].name, "#hashtag");
+    }
+
+    #[test]
+    fn should_find_two_hashtags_in_text() {
+        let hashtags = parse("text #hashtag1 https://google.com/?test#hash #hashtag2 http://google.pl/ text");
+
+        assert_eq!(hashtags.len(), 2);
+        assert_eq!(hashtags[0].name, "#hashtag1");
+        assert_eq!(hashtags[1].name, "#hashtag2");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 extern crate regex;
 
 mod links;
+mod hashtags;
 
 #[derive(Debug)]
 pub struct MessageMeta {
     pub message: String,
     pub links: Vec<links::Link>,
+    pub hashtags: Vec<hashtags::Hashtag>,
 }
 
 pub fn parse<T: ToString>(msg: T) -> MessageMeta {
     let message = msg.to_string();
     let links = links::parse(message.clone());
+    let hashtags = hashtags::parse(message.clone());
 
-    MessageMeta { message, links }
+    MessageMeta { message, links, hashtags }
 }
 
 #[cfg(test)]
@@ -28,11 +31,13 @@ mod tests {
     }
 
     #[test]
-    fn should_parse_message_with_link() {
-        let meta = parse("test message https://google.com/ test");
+    fn should_parse_with_all_features() {
+        let meta = parse("test message https://google.com/ #hashtag test");
 
-        assert_eq!(meta.message, "test message https://google.com/ test");
+        assert_eq!(meta.message, "test message https://google.com/ #hashtag test");
         assert_eq!(meta.links.len(), 1);
         assert_eq!(meta.links[0].url, "https://google.com/");
+        assert_eq!(meta.hashtags.len(), 1);
+        assert_eq!(meta.hashtags[0].name, "#hashtag");
     }
 }
